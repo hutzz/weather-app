6b816cc911a64df2654bcb99e652a3bce845719f
Zach Hutz <zachhutz0@gmail.com>
2022-01-11 22:51:43 -0500

    working version with current weather

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Current from "./Current.js";
+import "./index.css";
+const App = () => {
+	return (
+		<React.Fragment>
+			<Current />
+		</React.Fragment>
+	);
+};
+export default App;
