ec9b6c7552761bb5976dccb74f26b6b2ee4ddb80
Zach Hutz <zachhutz0@gmail.com>
2022-02-21 15:16:49 -0500

    Added routing for all pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Navbar from "./Navbar.js";
 import Current from "./Current.js";
+import SevenDay from "./SevenDay.js";
+import Error from "./Error.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
+
 const App = () => {
 	return (
-		<React.Fragment>
-			<Current />
-		</React.Fragment>
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Current />} />
+				<Route path='/sevenday' element={<SevenDay />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</Router>
 	);
 };
 export default App;
