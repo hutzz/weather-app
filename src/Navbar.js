82825bb40a5f7d6d07e151b4557297d2942a72a2
Zach Hutz <zachhutz0@gmail.com>
2022-02-21 15:14:06 -0500

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+	let navigate = useNavigate();
+	return (
+		<>
+			<div id='nav-wrapper'>
+				<button
+					onClick={() => {
+						navigate("/");
+					}}
+				>
+					CURRENT
+				</button>
+				<button
+					onClick={() => {
+						navigate("/sevenday");
+					}}
+				>
+					FORECAST
+				</button>
+			</div>
+		</>
+	);
+};
+export default Navbar;
---
+++ src/objEmpty.js
@@ -0,0 +1,4 @@
+const objEmpty = (obj) => {
+	return Object.keys(obj).length === 0;
+};
+export default objEmpty;
