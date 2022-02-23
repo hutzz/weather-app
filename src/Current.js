7fc0bcec352a4e32e78d412ca43fc4557bb7c8ff
Zach Hutz <zachhutz0@gmail.com>
2022-02-23 13:29:32 -0500

    Cleaned up code

--- src/Current.js
+++ src/Current.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import getCoords from "./getCoords";
 import Loading from "./Loading";
 import objEmpty from "./objEmpty";
+import windDir from "./windDir";
 import "./index.css";
 
+const UnitContext = React.createContext();
+
 const Current = ({ unit }) => {
-	const [weather, setWeather] = useState({});
-	const [main, setMain] = useState({});
-	const [wind, setWind] = useState({});
-	const [sun, setSun] = useState({});
+	const [weatherData, setWeatherData] = useState({});
 
 	useEffect(() => {
 		const currentWeather = async () => {
@@ -18,99 +18,160 @@ const Current = ({ unit }) => {
 				`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${process.env.REACT_APP_API_KEY}`
 			);
 			const data = await response.json();
-			setWeather({
-				id: data.weather[0].id,
-				main: data.weather[0].main,
-				desc: data.weather[0].description,
-				icon: data.weather[0].icon,
-			});
-			setMain({
-				currentTemp: data.main.temp,
-				feelsLike: data.main.feels_like,
-				humidity: data.main.humidity,
-				pressure: data.main.pressure,
-				min: data.main.temp_min,
-				max: data.main.temp_max,
-			});
-			setWind({
-				speed: data.wind.speed,
-				direction: data.wind.deg,
-				gust: data.wind.gust,
-			});
-			setSun({
-				sunrise: data.sys.sunrise,
-				sunset: data.sys.sunset,
-			});
+			console.log(data);
+			setWeatherData(data);
 		};
 		currentWeather();
 	}, [unit]);
-	if (objEmpty(weather) || objEmpty(main) || objEmpty(wind) || objEmpty(sun))
+	if (objEmpty(weatherData))
 		return (
 			<>
 				<Loading />
 			</>
 		);
 	return (
-		<>
+		<UnitContext.Provider value={unit}>
 			<div className='pane'>
-				<div id='cur-weather-wrapper'>
+				<Main
+					main={weatherData.weather[0].main}
+					desc={weatherData.weather[0].description}
+					icon={weatherData.weather[0].icon}
+					currentTemp={weatherData.main.temp}
+					feelsLike={weatherData.main.feels_like}
+					humidity={weatherData.main.humidity}
+					pressure={weatherData.main.pressure}
+					min={weatherData.main.temp_min}
+					max={weatherData.main.temp_max}
+					speed={weatherData.wind.speed}
+					direction={weatherData.wind.deg}
+					sunrise={weatherData.sys.sunrise}
+					sunset={weatherData.sys.sunset}
+				/>
+			</div>
+		</UnitContext.Provider>
+	);
+};
+const Main = ({
+	main,
+	desc,
+	icon,
+	currentTemp,
+	feelsLike,
+	humidity,
+	pressure,
+	min,
+	max,
+	speed,
+	direction,
+	sunrise,
+	sunset,
+}) => {
+	// may consider useContext for these instead of prop drilling, but for now this will do
+	return (
+		<>
+			<div id='cur-weather-wrapper'>
+				<Temps
+					main={main}
+					desc={desc}
+					icon={icon}
+					currentTemp={currentTemp}
+					feelsLike={feelsLike}
+					min={min}
+					max={max}
+				/>
+				<div id='cur-humidity'>{humidity}% Humidity</div>
+				<div id='cur-pressure'>{pressure} hPa</div>
+				<Wind speed={speed} direction={direction} />
+			</div>
+			<Sun sunrise={sunrise} sunset={sunset} />
+		</>
+	);
+};
+const Temps = ({ main, desc, icon, currentTemp, feelsLike, min, max }) => {
+	const unitData = useContext(UnitContext);
+	return (
+		<>
+			{" "}
+			<img
+				id='cur-icon'
+				src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+				alt={desc}
+			/>
+			<div id='cur-weather'>{main}</div>
+			<div id='cur-temp'>
+				{Math.round(currentTemp) +
+					(unitData === "metric" ? "°C" : "°F")}
+			</div>
+			<div id='cur-feelslike'>
+				Feels like{" "}
+				{Math.round(feelsLike) + (unitData === "metric" ? "°C" : "°F")}
+			</div>
+			<div id='low-high-wrapper'>
+				<div id='cur-low-wrapper'>
 					<img
-						id='cur-icon'
-						src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
-						alt={weather.desc}
+						className='current-high-low-icon'
+						src={require("./extras/low.png")}
+						alt='LOW'
 					/>
-					<div id='cur-weather'>{weather.main}</div>
-					<div id='cur-temp'>
-						{Math.round(main.currentTemp) +
-							(unit === "metric" ? "°C" : "°F")}
-					</div>
-					<div id='cur-feelslike'>
-						Feels like{" "}
-						{Math.round(main.feelsLike) +
-							(unit === "metric" ? "°C" : "°F")}
-					</div>
-				</div>
-				<div id='low-high-wrapper'>
 					<div id='cur-low'>
-						Low:{" "}
-						{Math.round(main.min) +
-							(unit === "metric" ? "°C" : "°F")}
-					</div>
-					<div id='cur-high'>
-						High:{" "}
-						{Math.round(main.max) +
-							(unit === "metric" ? "°C" : "°F")}
-					</div>
-					<div id='cur-humidity'>{main.humidity}% Humidity</div>
-					<div id='cur-pressure'>{main.pressure} hPa</div>
-					<div id='cur-speed'>
-						Wind speed:{" "}
-						{unit === "metric"
-							? Math.round(wind.speed * 3.6) + " km/h"
-							: Math.round(wind.speed) + " mph"}{" "}
+						{Math.round(min) +
+							(unitData === "metric" ? "°C" : "°F")}
 					</div>
 				</div>
-				<div id='sun-wrapper'>
-					<div id='cur-sunrise'>
-						<img
-							className='sunimg'
-							src={require("./extras/rise.png")}
-							alt='Sunrise'
-						/>
-						{new Date(sun.sunrise * 1000).toLocaleTimeString([], {
-							timeStyle: "short",
-						})}
+				<div id='cur-high-wrapper'>
+					<img
+						className='current-high-low-icon'
+						src={require("./extras/high.png")}
+						alt='HIGH'
+					/>{" "}
+					<div id='cur-high'>
+						{Math.round(max) +
+							(unitData === "metric" ? "°C" : "°F")}
 					</div>
-					<div id='cur-sunset'>
-						<img
-							className='sunimg'
-							src={require("./extras/set.png")}
-							alt='Sunset'
-						/>
-						{new Date(sun.sunset * 1000).toLocaleTimeString([], {
-							timeStyle: "short",
-						})}
-					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+const Wind = ({ speed, direction }) => {
+	const unitData = useContext(UnitContext);
+	return (
+		<>
+			<div id='cur-speed'>
+				<img src={require("./extras/wind.png")} alt='WIND SPEED' />
+				<br />
+				{unitData === "metric"
+					? Math.round(speed * 3.6) + " km/h "
+					: Math.round(speed) + " mph "}
+				{windDir(direction)}
+			</div>
+		</>
+	);
+};
+const Sun = ({ sunrise, sunset }) => {
+	return (
+		<>
+			{" "}
+			<div id='sun-wrapper'>
+				<div id='cur-sunrise'>
+					<img
+						className='sunimg'
+						src={require("./extras/rise.png")}
+						alt='Sunrise'
+					/>
+					{new Date(sunrise * 1000).toLocaleTimeString([], {
+						timeStyle: "short",
+					})}
+				</div>
+				<div id='cur-sunset'>
+					<img
+						className='sunimg'
+						src={require("./extras/set.png")}
+						alt='Sunset'
+					/>
+					{new Date(sunset * 1000).toLocaleTimeString([], {
+						timeStyle: "short",
+					})}
 				</div>
 			</div>
 		</>
